2abfbbdee925d6a08fa844ff7d357c36351ee60a
nick ortman <ortnick@gmail.com>
2026-04-09 11:49:33 -0500

    update

--- FoodReviews.sql
+++ FoodReviews.sql
@@ -7,13 +7,14 @@ CREATE TABLE Food_Reviews (
     ReviewID INT AUTO_INCREMENT PRIMARY KEY,
     FoodName VARCHAR(100) NOT NULL,
     EaseOfMaking DECIMAL(3,1) CHECK (EaseOfMaking >= 0.0 AND EaseOfMaking <= 10.0),
-    Taste DECIMAL(3,1) CHECK (Taste >= 0.0 AND Taste <= 10.0)
+    Taste DECIMAL(3,1) CHECK (Taste >= 0.0 AND Taste <= 10.0),
+    DiataryClass INT NOT NULL
 );
 
-INSERT INTO Food_Reviews (FoodName, EaseOfMaking, Taste)
+INSERT INTO Food_Reviews (FoodName, EaseOfMaking, Taste, DiataryClass)
 VALUES
-    ('Butter chicken', 9.4, 8.6),
-    ('Scallion Pancakes', 9.2, 6.3),
-    ('Vodka Pasta', 9.6, 9.5),
-    ('Thai Red Curry', 9.3, 9.2),
-    ('Pad Kra Pao', 9.1, 9.2);
+    ('Butter chicken', 9.4, 8.6, 1),
+    ('Scallion Pancakes', 9.2, 6.3, 3),
+    ('Vodka Pasta', 9.6, 9.5, 2),
+    ('Thai Red Curry', 9.3, 9.2, 1),
+    ('Pad Kra Pao', 9.1, 9.2, 1);
